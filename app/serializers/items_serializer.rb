91663f168a22dee552f9a28a6663f795c9a56825
Chris Romero <chrisleeromero1@gmail.com>
2022-05-17 19:51:57 -0600

    feat: items index page displays items

---
+++ app/serializers/items_serializer.rb
@@ -0,0 +1,17 @@
+class ItemsSerializer
+  def self.format_items(items)
+    {
+      data: items.map do |item|
+        {
+          id: item.id.to_s,
+          type: 'item',
+          attributes: {
+            name: item.name,
+            description: item.description,
+            unit_price: item.unit_price
+          }
+        }
+    end
+    }
+  end
+end
