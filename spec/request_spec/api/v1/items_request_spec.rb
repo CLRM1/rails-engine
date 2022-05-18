024a99146532db89400ba598a2d6ebad1cacd161
Chris Romero <chrisleeromero1@gmail.com>
2022-05-17 19:28:04 -0600

    test: items routes should return formatted JSON

--- spec/request_spec/api/v1/items_request_spec.rb
+++ spec/request_spec/api/v1/items_request_spec.rb
@@ -1,11 +1,28 @@
-# require 'rails_helper'
-#
-# RSpec.describe 'Items API' do
-#   it 'sends a list of items' do
-#     create_list(:item, 2)
-#
-#     get '/api/v1/items'
-#
-#     expect(response).to be_successful
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe 'Items API' do
+  it 'sends a list of items' do
+    create_list(:item, 2)
+
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+    expect(response.body).to include(Item.first.id.to_s)
+  end
+
+  it 'returns attributes for a single item' do
+    create_list(:item, 2)
+
+    get "/api/v1/items/#{Item.first.id}"
+
+    expect(response.body).to include(Item.first.id.to_s)
+    expect(response).to be_successful
+  end
+
+  it 'returns an empty array when there are no items' do
+
+    get "/api/v1/items"
+
+    expect(response).to be_successful
+  end
+end
