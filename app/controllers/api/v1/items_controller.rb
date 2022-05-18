6b63b8528bfabe8f9f90f983f693bdeeeff1a2d8
Chris Romero <chrisleeromero1@gmail.com>
2022-05-17 19:58:38 -0600

    feat: item show page passes both postman tests and rspec

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::ItemsController < ApplicationController
   def index
     render json: ItemsSerializer.format_items(Item.all), status: :ok
   end
+
+  def show
+    render json: ItemsSerializer.format_item(Item.find(params[:id])), status: :ok
+  end
 end
--- app/serializers/items_serializer.rb
+++ app/serializers/items_serializer.rb
@@ -1,17 +1,38 @@
 class ItemsSerializer
   def self.format_items(items)
+    if Item.all.count > 0
+      {
+        data: items.map do |item|
+          {
+            id: item.id.to_s,
+            type: 'item',
+            attributes: {
+              name: item.name,
+              description: item.description,
+              unit_price: item.unit_price,
+              merchant_id: item.merchant_id
+            }
+          }
+      end
+      }
+    else
+      []
+    end
+  end
+
+  def self.format_item(item)
     {
-      data: items.map do |item|
+      data:
         {
           id: item.id.to_s,
           type: 'item',
           attributes: {
             name: item.name,
             description: item.description,
-            unit_price: item.unit_price
+            unit_price: item.unit_price,
+            merchant_id: item.merchant_id
           }
         }
-    end
     }
   end
 end
