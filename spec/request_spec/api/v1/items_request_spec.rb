8d62d40015336f2a2d9955a21171ab087ffc7408
Chris Romero <chrisleeromero1@gmail.com>
2022-05-19 19:11:54 -0600

    test: min_price should return items with that price or higher

--- spec/request_spec/api/v1/items_request_spec.rb
+++ spec/request_spec/api/v1/items_request_spec.rb
@@ -221,4 +221,37 @@ RSpec.describe 'Items API' do
     expect(response.body).to_not include(item_2.name)
     expect(response.body).to_not include(item_3.name)
   end
+
+  it 'returns all items with a specified minimum price' do
+    merchant = Merchant.create!(name: 'Chris')
+    item = merchant.items.create!(
+      name: 'ball',
+      description: 'You can throw it.',
+      unit_price: 10,
+      merchant_id: merchant.id)
+
+    item_1 = merchant.items.create!(
+      name: 'stall',
+      description: 'You can put things in it.',
+      unit_price: 150,
+      merchant_id: merchant.id)
+
+    item_2 = merchant.items.create!(
+      name: 'the board game',
+      description: 'You can play with it.',
+      unit_price: 20,
+      merchant_id: merchant.id)
+
+    item_3 = merchant.items.create!(
+      name: 'fan',
+      description: 'It blows air.',
+      unit_price: 9.99,
+      merchant_id: merchant.id)
+      get "/api/v1/items/find_all?min_price=10"
+
+    expect(response.body).to include(item.name)
+    expect(response.body).to include(item_1.name)
+    expect(response.body).to include(item_2.name)
+    expect(response.body).to_not include(item_3.name)
+  end
 end
